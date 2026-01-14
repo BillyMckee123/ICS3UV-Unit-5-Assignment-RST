6fc956a9bb437dbc5aa8ce617ead5cd3ffdeb62f
default <billy.mckee@stu.ocsb.ca>
2026-01-14 14:15:12 +0000

    added code to main.ts file

--- main.ts
+++ main.ts
@@ -12,3 +12,21 @@ function createDeck(): void {
     }
   }
 }
+// ===== Part 2: Shuffle and Deal =====
+
+function shuffleDeck(): void {
+  for (let i = deck.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+  }
+}
+
+function dealCards(amount: number): string[] {
+  let hand: string[] = [];
+  for (let i = 0; i < amount; i++) {
+    hand.push(deck.pop() as string);
+  }
+  return hand;
+}
